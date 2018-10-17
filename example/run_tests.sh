ebb931c50f77e30b6f77cd79a9047a373c9fcf52
Matthew Paletta <mattpaletta@gmail.com>
2018-10-17 13:12:23 -0700

    fixed tests

--- example/run_tests.sh
+++ example/run_tests.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-test0="$(python3 ex_basic.py | tail -n 1)"
-cat $test0
+python ex_basic.py
 
-test1="$(python3 ex_basic.py --a 1 --b 2 | tail -n 1)"
-cat $test1
+python ex_basic.py --a 1 --b 2
 
-test2="$(python3 ex_config.py --config config.yml | tail -n 1)"
-cat $test2
+python ex_config.py --config config.yml
 
-test3="$(python3 ex_config.py -a 1 --config config.yml | tail -n 1)"
-cat $test3
+python ex_config.py -a 1 --config config.yml
