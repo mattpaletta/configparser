a2a562080067f8a69830d1bb69097e3be03fd5e3
Matthew Paletta <mattpaletta@gmail.com>
2019-01-20 21:26:58 -0800

    Bumped to v0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
         name = "config_parser",
-        version = "0.0.1",
+        version = "0.0.2",
         url = 'https://github.com/mattpaletta/configs',
         packages = find_packages(),
         include_package_data = True,
